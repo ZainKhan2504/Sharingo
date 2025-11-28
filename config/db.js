c316fccec767f498ad857106fea2a924dffaba05
zaikh847 <zaikh847@student.liu.se>
2025-11-28 15:35:39 +0500

    Fix MongoDB deprecation warnings by removing deprecated connection options

--- config/db.js
+++ config/db.js
@@ -5,9 +5,6 @@ const db = config.get("mongoURI");
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
-      // Getting rid of deprecation warnings
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
       useCreateIndex: true,
       useFindAndModify: false,
     });
